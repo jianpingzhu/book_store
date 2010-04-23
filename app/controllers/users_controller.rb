e54046cfcca91566972c3c297387db7d3bd4417d
jianpingzhu <ptzhu@yahoo.com>
2010-04-23 15:52:56 -0700

    User signup complete again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
       redirect_to @new_user
     else
       @title = "Sign Up"
+      @new_user.password = ''
+      @new_user.password_confirmation = ''
       render 'new'
     end
   end
